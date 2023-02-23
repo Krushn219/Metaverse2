053ea506a7624a28b634afcfac7438c70a7df75f
Krushn219 <krushn.219@gmail.com>
2023-02-23 15:42:40 +0530

    fixing img

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -5,11 +5,7 @@ const NewFeatures = ({ imgUrl, title, subtitle }) => (
     <div
       className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}
     >
-      <img
-        src={`${process.env.NEXT_PUBLIC_FAVICON}${imgUrl}`}
-        alt="icon"
-        className="w-1/2 h-1/2 object-contain"
-      />
+      <img src={imgUrl} alt="icon" className="w-1/2 h-1/2 object-contain" />
     </div>
     <h1 className="mt-[26px] font-bold text-[24px] leading-[30.24px] text-white">
       Title {title}
